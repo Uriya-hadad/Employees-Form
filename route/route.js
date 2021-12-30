98bee11c36ad26357270acc0c5c5b0e7e28a9101
Uriya-hadad <80356969+Uriya-hadad@users.noreply.github.com>
2021-12-30 17:25:20 +0200

    Delete route directory

--- route/route.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getHello,clearDb} = require('../controllers/routesFunctions');
-router.route('/').post(getHello).delete(clearDb);
-
-module.exports = router;
